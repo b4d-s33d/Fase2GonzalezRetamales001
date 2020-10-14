e90760a51f8c78c05afc3d75564bd4d163c1b31d
Wladi <jes.gonzalezr@alumnos.duoc.cl>
2020-10-14 18:54:40 -0300

    BookStop: Creando Modelos

--- locallibrary/login/models.py
+++ locallibrary/login/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
+import uuid # Required for unique book instances
 
 # Create your models here.
+class Genero(models.Model):
+    #Model representing a book genre."""
+	nombre = models.CharField(max_length=200)
+	
+	def __str__(self):
+		return self.nombre
+
+class Libro(models.Model):
+    
+	titulo = models.CharField(max_length=200)
+	autor = models.ForeignKey('Autor', on_delete=models.SET_NULL, null=True)   
+	resumen = models.TextField(max_length=1000, help_text='Ingrese una breve descripción del libro')
+	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="isbn-international.org/content/what-isbn">ISBN number</a>')
+	genero = models.ManyToManyField(Genero)
+    
+	def __str__(self):
+		return self.titulo
+    
+	def get_absolute_url(self):
+		"""Returns the url to access a detail record for this book."""
+		return reverse('book-detail', args=[str(self.id)])
+
+class Autor(models.Model):
+	"""Model representing an author."""
+	nombre = models.CharField(max_length=100)
+	apellido = models.CharField(max_length=100)
+	fecha_nac = models.DateField(null=True, blank=True)
+	fecha_fall = models.DateField('Died', null=True, blank=True)
+
+	class Meta:
+		ordering = ['apellido', 'nombre']
+
+	def get_absolute_url(self):
+		return reverse('author-detail', args=[str(self.id)])
+
+	def __str__(self):
+		"""String for representing the Model object."""
+		return f'{self.apellido}, {self.nombre}'	        
