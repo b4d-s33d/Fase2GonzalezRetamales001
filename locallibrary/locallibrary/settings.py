353ab145980415b4b27ce8e58661ee96a01bd040
Emforus <jretamales.vel@gmail.com>
2020-10-28 09:01:26 -0300

    BookStop: Creando Pagina de Inicio

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'login',
+    'login.apps.LoginConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- locallibrary/login/views.py
+++ locallibrary/login/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Libro, Autor, Genero
 
 # Create your views here.
+def index(request):
+    num_books = Libro.objects.all().count()
+    num_authors = Autor.objects.all().count()
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books, 'num_authors':num_authors,}
+    )
